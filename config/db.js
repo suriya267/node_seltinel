ef46f9ff2b40d5ea3c5eace1d542722f01eb9961
suriya267 <suriyasv4021@gmail.com>
2025-06-12 14:44:10 +0530

    script changes

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDb = async () => {
-  try {
-    console.log("process.env.MONGODB_URI : ",process.env.MONGODB_URI);
-    
+  try {    
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (err) {
